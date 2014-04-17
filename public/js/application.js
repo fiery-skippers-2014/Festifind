5efbef17f7a7df455a84c82315820c4bb7ea174d
Apprentice <apprentice@dbc20.local>
2014-04-17 01:53:30 -0700

    Search field now returns list of band's singles from Spotify API   *Added JavaScript logic in application.js

--- public/js/application.js
+++ public/js/application.js
@@ -1,6 +1,18 @@
 $(document).ready(function () {
 
   // Optional - Use AJAX to send an HTTP DELETE request for the sign-out link
-  
+  $('button').on('click', function() {
+    var search_params = $('#search_field').val();
+
+    $.ajax({
+      url: '/search_results',
+      type: 'POST',
+      data: {query: search_params }
+    }).done(function(response) {
+      $.parseJSON(response).albums.forEach(function(album){
+        $('.results').append('<li>' + album + '</li>')
+      })
+    })
+  })
 
 });
